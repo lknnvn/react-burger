3d9098d25c2c5060b903918ba868f0071cab0fb5
Ivan L <lknnvn@gmail.com>
2024-01-31 20:52:25 +0300

    fix 2 from review

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -10,38 +10,45 @@ const AppHeader: React.FC<AppHeaderProps> = () => {
 
     return (
         <header className={styles.header}>
-            <nav className={styles.container}>
+            <nav className={styles.nav}>
 
-                <a
-                    href="/constructor"
-                    className={`${styles.link} ${styles.linkActive} mr-2`}
-                >
-                    <BurgerIcon type='primary'/>
-                    <span className="pl-2">Конструктор</span>
-                </a>
-
-                <a
-                    href="/order-list"
-                    className={styles.link}
-                >
-                    <ListIcon type='secondary'/>
-                    <span className="pl-2">Лента заказов</span>
-                </a>
-
-                <a
-                    href="/"
-                    className={styles.logo}
-                >
-                    <Logo/>
-                </a>
-
-                <a
-                    href="/user"
-                    className={styles.link}
-                >
-                    <ProfileIcon type='secondary'/>
-                    <span className="pl-2">Личный кабинет</span>
-                </a>
+                <ul>
+                    <li>
+                        <a
+                            href="/constructor"
+                            className={`${styles.link} ${styles.linkActive} mr-2`}
+                        >
+                            <BurgerIcon type='primary'/>
+                            <span className="pl-2">Конструктор</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="/order-list"
+                            className={styles.link}
+                        >
+                            <ListIcon type='secondary'/>
+                            <span className="pl-2">Лента заказов</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="/"
+                            className={styles.logo}
+                        >
+                            <Logo/>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="/user"
+                            className={styles.link}
+                        >
+                            <ProfileIcon type='secondary'/>
+                            <span className="pl-2">Личный кабинет</span>
+                        </a>
+                    </li>
+                </ul>
 
             </nav>
         </header>
